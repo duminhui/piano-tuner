6195dee71eea2938b2fea124e6c149ea0f19c9b9
Du Minhui <duminghui@126.com>
2015-06-06 20:50:56 +0800

    modify tuner scheme

--- models/tuner.js
+++ models/tuner.js
@@ -1,5 +1,13 @@
 var mongoose = require('mongoose');
 
+var TuningItemSchema = new mongoose.Schema({
+    piano_tuning: Number,
+    piano_repair: Number,
+    action_regulation: Number,
+    hammer_voicing: Number
+});
+    
+
 var TunerSchema = new mongoose.Schema({
     first_name: String,
     last_name: String,
@@ -7,9 +15,11 @@ var TunerSchema = new mongoose.Schema({
     age: Number,
     avatar: String,
     phone_number: String,
-    years_of_actual_operation: Number,
-    years_of_continuous_working: Number
-}
+    email_address: String,
+    years_of_entire_work: Number,
+    years_of_continuous_work: Number,
+    averange_orders_per_month: Number
+});
 
 var Tuner = mongoose.model('Tuner', TunerSchema);
 var a_tuner = new Costomer({  });
